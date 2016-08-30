7fa37168f999bb48084d640dcecc72c2e27468a0
kaiguogit <kaiguo1988@gmail.com>
2016-08-29 21:23:03 -0700

    changed filter hover

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,17 @@
 $(function(){
 
   //filter exercises 
-  $("#exercises .filters").on("click", "a", function(){
+  $("#exercises .filter").on("click", "a", function(){
     event.preventDefault();
     var rows = $("#exercises tbody > tr");
-    var filter = this.className;
+    var filter = $(this).data("filter");
     if(filter !== ""){
-     filter = "." + filter; 
+      filter = "." + filter; 
     }
+
+    $("#exercises .filter>li>a").removeClass("filter-selected");
+    $(this).addClass("filter-selected");
+
     rows.each(function(i, row){
       var label = $(row).find("span"+filter);
       if(label.length > 0){
@@ -23,13 +27,16 @@ $(function(){
   });
   
   //filter projects
-  $("#projects .filters").on("click", "a", function(){
+  $("#projects .filter").on("click", "a", function(){
     event.preventDefault();
     var projects = $(".project");
-    var filter = this.className;
+    var filter = $(this).data("filter");
     if(filter !== ""){
-     filter = "." + filter; 
+      filter = "." + filter; 
     }
+    $("#projects .filter>li>a").removeClass("filter-selected");
+    $(this).addClass("filter-selected");
+
     projects.each(function(i, project){
       var label = $(project).find("span"+filter);
       if(label.length > 0){
