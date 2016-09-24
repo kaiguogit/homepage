1b06a6b9124f49f8cce9579073ea37d4a1b54cc9
kaiguogit <git@kaiguo.ca>
2016-09-24 07:39:58 -0700

    added verbal alarm project

--- javascript/script.js
+++ javascript/script.js
@@ -4,15 +4,21 @@ $(function(){
   $("#exercises .filter").on("click", "a", function(){
     event.preventDefault();
     var rows = $("#exercises tbody > tr");
+
+    //get filter name from data-filter attribute, concat with . in front of it
+    //eg. data-filter="react"
+    //filter = ".react"
     var filter = $(this).data("filter");
     if(filter !== ""){
       filter = "." + filter; 
     }
-
     $("#exercises .filter>li>a").removeClass("filter-selected");
     $(this).addClass("filter-selected");
 
     rows.each(function(i, row){
+
+      //Find span with class name email to filter
+      //Eg. span.react 
       var label = $(row).find("span"+filter);
       if(label.length > 0){
         $(row).removeClass("flippedup");
@@ -30,14 +36,21 @@ $(function(){
   $("#projects .filter").on("click", "a", function(){
     event.preventDefault();
     var projects = $(".project");
+
+    //get filter name from data-filter attribute, concat with . in front of it
+    //eg. data-filter="react"
+    //filter = ".react"
     var filter = $(this).data("filter");
     if(filter !== ""){
       filter = "." + filter; 
     }
+
     $("#projects .filter>li>a").removeClass("filter-selected");
     $(this).addClass("filter-selected");
 
     projects.each(function(i, project){
+      //Find span with class name email to filter
+      //Eg. span.react 
       var label = $(project).find("span"+filter);
       if(label.length > 0){
         $(project).removeClass("flippedup");
